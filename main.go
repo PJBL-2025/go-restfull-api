efb2f44c78c3bd2c8ce71ffba20a32c931f94ff4
el <desktopme@gmail.com>
2025-05-31 16:10:38 +0700

    update docker

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5500, http://127.0.0.1:5500, http://localhost:5173",
+		AllowOrigins:     "http://localhost:5500, http://127.0.0.1:5500, http://localhost:5173, http://localhost:8000, http://127.0.0.1:8000",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS,PATCH",
 		AllowHeaders:     "Origin, Content-Type, Accept,Authorization",
 		AllowCredentials: true,
